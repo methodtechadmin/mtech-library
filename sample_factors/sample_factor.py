ac7c60e47e90aa93c6b29847defb3ed9269eaceb
varad1methodtech <varad.panaskar@methodtech.in>
2026-03-25 14:48:15 +0530

    changes

---
+++ sample_factors/sample_factor.py
@@ -0,0 +1,41 @@
+
+from abstract_factor import AbstractFactor
+from mtech import Region,DateTime,CorporateActions,CorporateData,Universe,DateUtils,MarketData,constants as cns,columns as col
+import pandas as pd
+from mtech.enums import UniverseType, FinancialReportType, FinancialReportMetric,FinancialReportPeriod
+
+class SampleFactor(AbstractFactor):
+
+    def __init__(self, region: Region):
+        super().__init__(region)
+
+    def compute(self, date: DateTime) -> pd.DataFrame:
+        univ = Universe(self._region).get_universe(univ_type=UniverseType.NIFTY,date=date)
+        ukeys = univ[col.MSYMBOL_UKEY].tolist()
+
+        df_21 = MarketData().get_adjusted_price(
+            date_times=[DateUtils().shift_busdate(date, self._region, -cns.DAYS_IN_MONTH)],
+            ukeys=ukeys
+        )[[col.MSYMBOL_UKEY, col.CLOSE_PRICE]].rename(columns={col.CLOSE_PRICE: "price_21"})
+
+        df_252 = MarketData().get_adjusted_price(
+            date_times=[DateUtils().shift_busdate(date, self._region, -cns.DAYS_IN_YEAR)],
+            ukeys=ukeys
+        )[[col.MSYMBOL_UKEY, col.CLOSE_PRICE]].rename(columns={col.CLOSE_PRICE: "price_252"})
+
+        df = (
+            univ.merge(df_21, on=col.MSYMBOL_UKEY)
+                .merge(df_252, on=col.MSYMBOL_UKEY)
+        )
+        print(df)
+
+        df[col.SCORE] = df["price_21"]/df["price_252"] - 1
+
+        df = df[[col.DATETIME,col.MSYMBOL_UKEY,col.SCORE]]
+
+        return df
+
+
+if __name__ == "__main__":
+    factor = SampleFactor(Region("IN"))
+    print(factor.compute(DateTime("20200101")))
